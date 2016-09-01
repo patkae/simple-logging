2c1b28e92b88e2b43628327443c12090c0f0160d
patkae <pattaya1@gmail.com>
2016-09-01 13:26:01 -0400

    Added ConsoleLogProvider

---
+++ SimpleLogging/Logging/Providers/ConsoleLogProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleLogging.Logging.TextFormatters;
+
+namespace SimpleLogging.Logging.Providers
+{
+	public class ConsoleLogProvider : ILogProvider
+	{
+		private ITextFormatter _formatter = new TextFormatter();
+
+
+		public void Write(LogEntry entry)
+		{
+			Console.WriteLine(_formatter.Format(entry));
+		}
+	}
+}
