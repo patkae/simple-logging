7ce453986a3266567c4e192b007648e9af34c2af
patkae <pattaya1@gmail.com>
2016-09-06 09:43:41 -0400

    Added orphaned file

---
+++ SimpleLogging.UnitTests/FunctionLogger/LogFunction.cs
@@ -0,0 +1,14 @@
+﻿using SimpleLogging.Logging;
+
+namespace SimpleLogging.UnitTests.FunctionLogger
+{
+	public class LogFunction
+	{
+		public LogEntry Entry { get; set; }
+
+		public void WriteLog(LogEntry entry)
+		{
+			Entry = entry;
+		}
+	}
+}
